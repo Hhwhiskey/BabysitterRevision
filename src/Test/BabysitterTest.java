86b213a6a49769a66e33ab7d011019e1bea78551
Kevin <khfire22@gmail.com>
2016-02-20 16:08:02 -0500

    testMinimumPayout fail

--- src/Test/BabysitterTest.java
+++ src/Test/BabysitterTest.java
@@ -39,21 +39,35 @@ public class BabysitterTest {
         assertEquals(11, result);
     }
 
-    @Test public void testMinimumShiftLength() {
+    @Test
+    public void testMinimumShiftLength() {
         int result = babysitter.getMinimumShiftLength();
         assertEquals(1, result);
     }
 
-    @Test public void testMaximumPayout() {
+    @Test
+    public void testMaximumPayout() {
 
         // Daily max payout should not exceed $148
         // Which is 7 hours of day rate + 4 hours of late rate
         // with no bed time
-        
+
         int maxPayout = 148;
 
-        int result = babysitter.getMaxPayout();
+        int result = babysitter.getMaximumPayout();
         assertTrue(result <= maxPayout);
+    }
+
+    @Test
+    public void textMinimumPayout() {
+
+        // Daily min payout
+        // 1 hour of bedtime pay
+
+        int minimumPayout = 8;
+
+        int result = babysitter.getMinimumPayout();
+        assertTrue(result >= minimumPayout);
     }
 }
 
