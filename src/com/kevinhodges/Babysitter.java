b47d746ff3f19270f8958213c146f7b244e9f739
Kevin <khfire22@gmail.com>
2016-02-20 13:58:36 -0500

    testDayRate pass

--- src/com/kevinhodges/Babysitter.java
+++ src/com/kevinhodges/Babysitter.java
@@ -2,7 +2,13 @@ package com.kevinhodges;
 
 public class Babysitter {
 
+    int dayRate = 12;
+
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    public int getDayRate() {
+        return dayRate;
     }
 }
