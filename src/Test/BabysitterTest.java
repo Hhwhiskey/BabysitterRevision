7622362745403e519fe958a29069411db7ab6f92
Kevin <khfire22@gmail.com>
2016-02-20 16:23:36 -0500

    testCalculateShiftLength fail

--- src/Test/BabysitterTest.java
+++ src/Test/BabysitterTest.java
@@ -3,6 +3,9 @@ import com.kevinhodges.Babysitter;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Calendar;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -68,6 +71,29 @@ public class BabysitterTest {
 
         int result = babysitter.getMinimumPayout();
         assertTrue(result >= minimumPayout);
+    }
+
+    @Test
+    public void testCalculateShiftLength() {
+
+        // This test should return a shift length of 10 hours
+
+        // Set calendar object to time of 1700 on feb 19th
+        Calendar shiftStartDate = Calendar.getInstance();
+        shiftStartDate.set(Calendar.YEAR, 2016);
+        shiftStartDate.set(Calendar.MONTH, 2);
+        shiftStartDate.set(Calendar.DATE, 19);
+        shiftStartDate.set(Calendar.HOUR_OF_DAY, 17);
+
+        // Set calendar object to time of 0300 on feb 20th
+        Calendar shiftEndDate = Calendar.getInstance();
+        shiftEndDate.set(Calendar.YEAR, 2016);
+        shiftEndDate.set(Calendar.MONTH, 2);
+        shiftEndDate.set(Calendar.DATE, 20);
+        shiftEndDate.set(Calendar.HOUR_OF_DAY, 3);
+
+        int result = babysitter.calculateShiftLength(shiftStartDate, shiftEndDate);
+        assertEquals(10, result);
     }
 }
 
