a79e931ccf4fb193d60c4d479d870f07483ce4ce
Kevin <khfire22@gmail.com>
2016-02-20 13:55:34 -0500

    testDayRate fail

---
+++ src/Test/BabysitterTest.java
@@ -0,0 +1,24 @@
+package Test;
+import com.kevinhodges.Babysitter;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BabysitterTest {
+
+    private Babysitter babysitter;
+
+    @Before
+    public void setUp() {
+        babysitter = new Babysitter();
+    }
+
+    @Test
+    public void testDayRate() {
+        int result = babysitter.getDayRate();
+        assertEquals(12, result);
+    }
+}
+
+
---
+++ src/com/kevinhodges/Babysitter.java
@@ -0,0 +1,8 @@
+package com.kevinhodges;
+
+public class Babysitter {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
