5ff1a817c8bc582bce67ad8c2be05c56bd23e62a
Kevin <khfire22@gmail.com>
2016-02-20 16:28:13 -0500

    testCalculateShiftLength pass and variable refactor

--- src/com/kevinhodges/Babysitter.java
+++ src/com/kevinhodges/Babysitter.java
@@ -1,12 +1,15 @@
 package com.kevinhodges;
 
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
 public class Babysitter {
 
-    int dayRate = 12;
-    int bedRate = 8;
-    int lateRate = 16;
-    int maxShiftLength = 11;
-    int minimumShiftLength = 1;
+    public static int dayRate = 12;
+    public static int bedRate = 8;
+    public static int lateRate = 16;
+    public static int maxShiftLength = 11;
+    public static int minimumShiftLength = 1;
 
     public static void main(String[] args) {
     }
@@ -49,5 +52,37 @@ public class Babysitter {
         int minBedHours = 1;
 
         return minBedHours * bedRate;
+    }
+
+    // This method will return the length of shift in rounded hours
+    // after receiving two Calendar objects, shiftStartDate and shiftEndDate
+    public int calculateShiftLength(Calendar shiftStartDate, Calendar shiftEndDate) {
+
+        // Create calendar date with time of 1700 on feb 19th
+        shiftStartDate.set(Calendar.YEAR, 2016);
+        shiftStartDate.set(Calendar.MONTH, 2);
+        shiftStartDate.set(Calendar.DATE, 19);
+        shiftStartDate.set(Calendar.HOUR_OF_DAY, 17);
+
+        // Convert shiftStartDate to millis
+        long startingTime = shiftStartDate.getTimeInMillis();
+
+        // Create calendar date with time of 0300 on feb 20th
+        shiftEndDate.set(Calendar.YEAR, 2016);
+        shiftEndDate.set(Calendar.MONTH, 2);
+        shiftEndDate.set(Calendar.DATE, 20);
+        shiftEndDate.set(Calendar.HOUR_OF_DAY, 3);
+
+        // Convert shiftEndDate to millis
+        long quittingTime = shiftEndDate.getTimeInMillis();
+
+        // Subtract the starting time from quitting time to get the
+        // length of the shift in millis
+        long shiftLength = quittingTime - startingTime;
+
+        // Convert the millis time to hours, rounded up to the hour.
+        int shiftLengthHours = (int) TimeUnit.MILLISECONDS.toHours(shiftLength);
+
+        return shiftLengthHours;
     }
 }
